40941ff44a5c43c0d5d5ecb31886937f70c18ec2
Charlie Ikosi <charlieikosi@gmail.com>
2023-08-21 14:27:12 +1000

    updated

--- R/page_validate.R
+++ R/page_validate.R
@@ -1,3 +1,19 @@
+#' Page Validate
+#' 
+#' Function that validates the page argument in the pngnewsR package functions.
+#' @param pages Takes an integer value as input needing validation.
+#' @param url A web address as a character string or an object of character string. 
+#' @return If validation passes, function returns an integer or returns NULL if fails.
+#' @examples
+#' url <- "https://www.postcourier.com.pg/new-category/features-news/";
+#' validation_1 <- page_validate(1000000, url);
+#' validation_2 <- page_validate(1, url);
+#' @name page_validate
+#' @import rvest 
+#' @import tidyverse 
+#' @import tibble
+#' @export
+
 library(rvest)
 
 page_validate <- function(pages, url) {
@@ -19,8 +35,3 @@ page_validate <- function(pages, url) {
     return(NULL)
   }
 }
-
-# Examples
-#validation_1 <-page_validate(1000000, "https://www.postcourier.com.pg/new-category/features-news/")
-#validation_2 <-page_validate(1, "https://www.postcourier.com.pg/new-category/features-news/")
-
